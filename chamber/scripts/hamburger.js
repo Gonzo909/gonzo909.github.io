ee18d1f6f629b219e047183b34bd93c1335f43de
Gonzalo Jimenez <65154469+Gonzo909@users.noreply.github.com>
2023-10-09 11:47:48 -0600

    added stuff to chamber home page

---
+++ chamber/scripts/hamburger.js
@@ -0,0 +1,7 @@
+const hamButton= document.querySelector("#menu");
+const navbar= document.querySelector(".navbar");
+
+hamButton.addEventListener('click', () => {
+    navbar.classList.toggle('open');
+    hamButton.classList.toggle('open');
+});
