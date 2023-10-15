99b1bccd15e5f229f3328f6ac476b11000e73c67
Gonzalo Jimenez <65154469+Gonzo909@users.noreply.github.com>
2023-10-14 22:40:15 -0600

    added more styling to chamber home page

---
+++ chamber/scripts/darkmode.js
@@ -0,0 +1,14 @@
+const modeButton = document.querySelector("#mode");
+const body = document.querySelector("body");
+
+modeButton.addEventListener("click", () => {
+	if (modeButton.textContent.includes("🌑")) {
+		body.style.background = "#000";
+		body.style.color = "#fff";
+		modeButton.textContent = "☀️";
+	} else {
+		body.style.background = "#eee";
+		body.style.color = "#000";
+		modeButton.textContent = "🌑";
+	}
+});
