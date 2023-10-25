1b3584c92622fb721c33dbf4bff114f5317d54be
Gonzalo Jimenez <65154469+Gonzo909@users.noreply.github.com>
2023-10-25 15:44:53 -0600

    update home page

---
+++ scripts/visits.js
@@ -0,0 +1,14 @@
+const visitsDisplay = document.querySelector('.visits');
+
+let numVisits = Number(window.localStorage.getItem('numVisits-ls')) || 0;
+
+if (numVisits === 0) {
+    visitsDisplay.textContent = `This is your first visit.`;
+}
+else {
+    visitsDisplay.textContent = numVisits;
+}
+
+numVisits++;
+
+localStorage.setItem('numVisits-ls', numVisits);
