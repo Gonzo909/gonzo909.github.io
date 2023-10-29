022d9f447f4ab35960c280973f70ae25d2d11ed0
Gonzalo Jimenez <65154469+Gonzo909@users.noreply.github.com>
2023-10-29 17:23:57 -0600

    discover page updated with last visit message

---
+++ chamber/scripts/visitmessage.js
@@ -0,0 +1,17 @@
+const visitMessage = document.querySelector('.visitmessage');
+const msToDays = 84600000;
+const dateToday = new Date();
+
+let numVisits = Number(window.localStorage.getItem('numVisits-ls')) || 0;
+let lasVisit = new Date(window.localStorage.getItem('lastVisit-ls')) || 0;
+let now = Date.now();
+
+if (numVisits === 0) {
+    visitMessage.textContent = `Welcome! Let us know if you have any questions.`;
+}
+else if (numVisits >= 1 && (now - lasVisit) < (msToDays)) {
+    visitMessage.textContent = `Back so soon! Awesome!`;
+}
+else {
+    visitMessage.textContent = `You last visited ${Math.floor((now - lasVisit) / msToDays)} days ago.`;
+}
