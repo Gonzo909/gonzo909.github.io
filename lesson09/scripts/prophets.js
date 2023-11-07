ac4cf0d441a393b1d277fb5cf322e80f4c524a1a
Gonzalo Jimenez <65154469+Gonzo909@users.noreply.github.com>
2023-11-07 11:24:06 -0700

    added prophets

--- lesson09/scripts/prophets.js
+++ lesson09/scripts/prophets.js
@@ -2,9 +2,37 @@ const url = 'https://brotherblazzard.github.io/canvas-content/latter-day-prophet
 const cards = document.querySelector('.cards');
 
 async function getProphetData() {
-    await fetch(url);
+    const response = await fetch(url);
+    const data = response.json();
+    //console.table(data.prophets);
+    displayProphets(data.prophets);
 }
 
-const response = getProphetData();
-const data = response.json();
-console.table(data);
+const displayProphets = (prophets) => {
+    prophets.forEach((prophet) => {
+        let card = document.createElement('section');
+        let fullname = document.createElement('h2');
+        let birthdate = document.createElement('p');
+        let birthplace = document.createElement('p');
+        let portrait = document.createElement('img');
+        
+        fullname.textContent = `${prophet.name} ${prophet.lastname}`;
+        birthdate.textContent = `Date of Birth: ${prophet.birthdate}`;
+        birthplace.textContent = `Place of Birth: ${prophet.birthplace}`;
+
+        portrait.setAttribute('src', prophet.imageurl);
+        portrait.setAttribute('alt', `Portrait of ${prophet.name} ${prophet.lastname}`);
+        portrait.setAttribute('loading', 'lazy');
+        portrait.setAttribute('width', '340');
+        portrait.setAttribute('height', '440');
+
+        card.appendChild(fullname);
+        card.appendChild(portrait);
+        card.appendChild(birthdate);
+        card.appendChild(birthplace);
+
+        cards.appendChild(card);
+    });
+}
+
+getProphetData();
