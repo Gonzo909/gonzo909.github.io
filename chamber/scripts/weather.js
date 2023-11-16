c0f0696bdf8b2a1c8ae07fc18204d649af0efe90
Gonzalo Jimenez <65154469+Gonzo909@users.noreply.github.com>
2023-11-16 11:26:28 -0700

    WIP weather for chamber home page

---
+++ chamber/scripts/weather.js
@@ -0,0 +1,37 @@
+const weather = document.querySelector('#weather');
+const weatherIcon = document.querySelector('#weather-icon');
+const caption = document.querySelector('figcaption');
+const forecast = document.querySelector('#forecast');
+const url = "https://api.openweathermap.org/data/2.5/weather?lat=43.82&lon=-111.79&appid=f83e65ed402b6ebee8b09a46b89275a4&units=imperial";
+
+async function apiFetch() {
+    try {
+        const response = await fetch(url);
+        if (response.ok) {
+            const data = await response.json();
+            console.log(data);
+            displayResults(data);
+        }
+        else {
+            throw Error(await response.text());
+        }
+    }
+    catch (error) {
+        console.log("Error: ", error);
+    }
+}
+
+function displayResults(data) {
+    weather.textContent = `${data.main.temp.toFixed(0)}°F`;
+    const iconsrc = `https://openweathermap.org/img/w/${data.weather[0].icon}.png`;
+    let desc = data.weather[0].description;
+
+    weatherIcon.setAttribute('src', iconsrc);
+    weatherIcon.setAttribute('alt', desc);
+
+    caption.textContent = `${desc.charAt(0).toUpperCase()}${desc.slice(1)}`;
+
+    //let 
+}
+
+apiFetch();
